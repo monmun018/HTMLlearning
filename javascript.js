e07f5d30ce13c13d16bbfa9afbe83e1487deef52
monmun018 <59110447+monmun018@users.noreply.github.com>
2020-10-15 06:25:57 +0900

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,8 @@
+//メニューを開ける
+function openNav() {
+    document.getElementById("mySidenav").style.width = "100%";
+  }
+//メニューを閉じる
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
